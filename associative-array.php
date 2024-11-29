27a8ad551fb5e829c933f1fb7b5319978bee8a59
加藤田誠次 <seiji561016@gmail.com>
2024-11-29 23:54:20 +0900

    Add files via upload

---
+++ associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHPの基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $product = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        print_r($product);
+        ?>
+
+    </p>
+</body>
+</html>
+
+
+
